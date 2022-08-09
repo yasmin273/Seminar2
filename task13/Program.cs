044ce1e20808d35c163b52162e02c20ba8e2be46
Yasmin <jasminvk@yandex.ru>
2022-08-09 17:03:24 +0300

    task13done

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+/// Пробует взять третью цифру из входного числа в виде строки.
+
+bool TryParseThirdDigit(string num, out int result)
+{
+    result = default;
+    if (num.Length < 3)
+        return false;
+
+    result = int.Parse(
+        num.ElementAt(2) 
+    ); 
+
+    return true;
+}
+
+Console.Write("Введите число: ");
+var num = Console.ReadLine();
+
+if (TryParseThirdDigit(num, out int thirdDigit))
+    Console.WriteLine($"{num} -> {thirdDigit}");
+else
+    Console.WriteLine($"{num} -> третьей цифры нет");
